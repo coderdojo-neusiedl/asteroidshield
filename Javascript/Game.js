448833d37b2df559f91893fd308296cddaaba01d
Thomas Ederer <thomas@neusiedl.coderdojo.net>
2021-06-18 18:59:05 +0200

    started implementation of asteroids

--- Javascript/Game.js
+++ Javascript/Game.js
@@ -1,7 +1,9 @@
 var bases = [];
 var missiles = [];
+var asteroids = [];
 var nextMissileId = 1;
-
+var asteroidMaxY;
+   
 var getNearestBase = function getNearestBase(mouseX) {
    var currentMinimumDistance = width;
    var nearestBase;
@@ -16,9 +18,9 @@ var getNearestBase = function getNearestBase(mouseX) {
 };
 
 var drawGroundScene = function drawGroundScene() {
-   var groundY   = height * 0.95;
-   var x         = 0;
-   var y         = groundY;
+   var groundY = height * 0.95;
+   var x       = 0;
+   var y       = groundY;
 
    stroke('yellow');
    fill('yellow');
@@ -34,16 +36,31 @@ var drawGroundScene = function drawGroundScene() {
    
 };
 
+var createAsteroid = function createAsteroid() {
+   var startX = width * 0.05 + Math.random() * width * 0.9;
+   console.log(startX);
+   asteroids.push(new Asteroid({x: startX, y: 50}, {x: 100, y: asteroidMaxY}));
+};
+
 function setup() {
    createCanvas(600, 600);
    var leftBase  = new Base({x: width * 0.15, y: height * 0.85}, width * 0.1,  height * 0.05);
    var rightBase = new Base({x: width * 0.90, y: height * 0.85}, width * 0.15, height * 0.05);
    bases = [leftBase, rightBase];
+   asteroidMaxY = 0;
+   bases.forEach(base => {
+      var y = base.getCenter().y + base.getHeight();
+      if (asteroidMaxY < y) {
+         asteroidMaxY = y;
+      }
+   });
+   setTimeout(createAsteroid, Math.random() * 1000);
 };
 
 function draw() {
    background('black');
    drawGroundScene();
+   asteroids.forEach(asteroid => asteroid.draw());
    missiles.forEach(missile => missile.draw());
 };
 
